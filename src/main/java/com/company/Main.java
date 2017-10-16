842cbdaee91dffffbafd6cb0ace15e2d1122414b
Massimiliano <mrusso.pro@gmail.com>
2017-10-16 10:45:37 +0200

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.IOException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Ciao");
+
+    }
+}
+
+
+
+
