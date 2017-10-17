b0bc0424ebee00ad78eca8cba555bdffccc626fa
rusiano <mrusso.pro@gmail.com>
2017-10-17 16:26:12 +0200

    Create Tree Structure

--- src/main/java/com/company/Master.java
+++ src/main/java/com/company/Master.java
@@ -8,15 +8,26 @@ import org.apache.zookeeper.*;
 
 public class Master {
 
-    // some sample code by Valerio
+    private static ZooKeeper zoo;
+
     public static void main(String[] args) throws InterruptedException, KeeperException, IOException {
+
+        // setup connection to Zookeeper
+        setupConnection();
+
+        // create tree structure
+        createTreeStructure();
+
+    }
+
+    private static void setupConnection() throws IOException, InterruptedException {
 
         String host = "localhost:2181";
         int sessionTimeout = 3000;
         final CountDownLatch connectionLatch = new CountDownLatch(1);
 
         //create a connection
-        ZooKeeper zoo = new ZooKeeper(host, sessionTimeout , new Watcher() {
+        zoo = new ZooKeeper(host, sessionTimeout , new Watcher() {
 
             @Override
             public void process(WatchedEvent we) {
@@ -30,31 +41,45 @@ public class Master {
 
         connectionLatch.await(10, TimeUnit.SECONDS);
 
-        //create znode
-        zoo.create("/test", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        //create znode sequential
-        zoo.create("/test/sequential", "znode_sequential".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
-        //create znode ephemereal
-        zoo.create("/test/ephemeral", "znode_ephemeral".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+    }
 
+    private static void createTreeStructure() throws KeeperException, InterruptedException {
+
+        // create REQUEST node with two children: ENROLL and QUIT
+        zoo.create("/request", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zoo.create("/request/enroll", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zoo.create("/request/quit", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+        // create REGITRY node as protected: only master has access to users' information
         String auth = "user:pwd";
         zoo.addAuthInfo("digest", auth.getBytes());
-        //create protected node
-        zoo.create("/test/protected", "znode".getBytes(), ZooDefs.Ids.CREATOR_ALL_ACL, CreateMode.PERSISTENT);
-
-        //set a watcher
-        BasicWatcher w = new BasicWatcher();
-        zoo.getData("/test/ephemeral", w, null);
-
-        //fire the watcher
-        zoo.setData("/test/ephemeral", "changed value".getBytes(), -1);
-
-        //delete protected node
-        zoo.delete("/test/protected", -1);
+        zoo.create("/registry", "znode".getBytes(), ZooDefs.Ids.CREATOR_ALL_ACL, CreateMode.PERSISTENT);
 
     }
 }
 
 
+/** Valerio's code *********************************************************************************************
+ //create znode
+ zoo.create("/test", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+ //create znode sequential
+ zoo.create("/test/sequential", "znode_sequential".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+ //create znode ephemereal
+ zoo.create("/test/ephemeral", "znode_ephemeral".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 
+ String auth = "user:pwd";
+ zoo.addAuthInfo("digest", auth.getBytes());
+ //create protected node
+ zoo.create("/test/protected", "znode".getBytes(), ZooDefs.Ids.CREATOR_ALL_ACL, CreateMode.PERSISTENT);
+
+ //set a watcher
+ BasicWatcher w = new BasicWatcher();
+ //zoo.getData("/test/ephemeral", w, null);
+
+ //fire the watcher
+ //zoo.setData("/test/ephemeral", "changed value".getBytes(), -1);
+
+ //delete protected node
+ //zoo.delete("/test/protected", -1);
+ ***************************************************************************************************************/
 
