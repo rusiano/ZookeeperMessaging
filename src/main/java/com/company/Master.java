6bf829d98347331e1b708a90d8c4e1840b9a1041
rusiano <mrusso.pro@gmail.com>
2017-11-17 15:15:22 +0100

    Added console line control over deletion of the tree at startup; Added first implementation of unprocessed requests management (e.g. while master is down); Moved getOnlineUsers() because of wrong placement (Worker class suits better); Changes to tree management logic at startup: request children do not have to be deleted.

--- src/main/java/com/company/Master.java
+++ src/main/java/com/company/Master.java
@@ -10,30 +10,38 @@ class Master{
     private static Scanner input = new Scanner(System.in);
 
     private static ZooKeeper zoo;
-    private static Watcher watcher;
+    private static MasterWatcher watcher;
 
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
 
         zoo = ZooHelper.getConnection();    // Connects to ZooKeeper service
-        if (zoo == null) { System.out.println("zoo is null"); return;}
+        if (zoo == null) { ZooHelper.print("<ERROR> It was impossible to establish a connection to Zookeeper. Exiting..."); return;}
+
+        String auth = "user:pwd";
+        zoo.addAuthInfo("digest", auth.getBytes());
 
         watcher = new MasterWatcher(zoo);   //  Sets the master watcher
 
-/*
-        System.out.print("> Do you want to completely remove the previous tree structure (Y/N)? ");
-        String answer = input.nextLine().toUpperCase();
-        if (answer.equals("Y"))
-            removeTreeStructure(true);      // Removes previous tree structure
-        else if (answer.equals("N"))
-            removeTreeStructure(false);
-        else
+        do {
+            System.out.print("> Do you want to completely remove the previous tree structure (Y/N)? ");
+            String answer = input.nextLine().toUpperCase();
+
+            if (answer.equals("Y")) {
+                removeTreeStructure();      // Removes previous tree structure
+                createTreeStructure();
+                setWatchers();
+                break;
+            } else if (answer.equals("N")) {
+                manageUnprocessedRequests();
+                setWatchers();
+                break;
+            }
+
             System.out.println("<ERROR> " + answer + " is not a valid command. Please retry.");
-*/
 
-        removeTreeStructure(true);      // Removes previous tree structure
-        createTreeStructure();             // Creates a new clean tree structure
+        } while (true);
 
-        System.out.println("> Enter any key at any time to stop the master.");
+        System.out.println("<<< ENTER ANY KEY AT ANY TIME TO STOP THE MASTER. >>>");
         System.out.println("> Master is running...");
         input.nextLine();
         System.out.println("> Stopping the master...");
@@ -44,21 +52,15 @@ class Master{
      * @throws KeeperException -
      * @throws InterruptedException -
      */
-    private static void removeTreeStructure(boolean all) throws KeeperException, InterruptedException {
-
-        // Give authorization to delete nodes.
-        String auth = "user:pwd";
-        zoo.addAuthInfo("digest", auth.getBytes());
+    private static void removeTreeStructure() throws KeeperException, InterruptedException {
 
         // !!! REGISTRY and BACKUP do not have to be deleted because they permanently store registered users and unread
         //  messages respectively !!!
         deleteSubtree("/request");
         deleteSubtree("/online");
         deleteSubtree("/queue");
-        if (all) {
-            deleteSubtree("/registry");
-            deleteSubtree("/backup");
-        }
+        deleteSubtree("/registry");
+        deleteSubtree("/backup");
 
     }
 
@@ -78,16 +80,10 @@ class Master{
         zoo.create("/online", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         zoo.create("/queue", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 
-        // create REGISTRY and BACKUP node as protected: only master has access to users' information and stored messages
-        String auth = "user:pwd";
-        zoo.addAuthInfo("digest", auth.getBytes());
         try {
             zoo.create("/registry", "znode".getBytes(), ZooDefs.Ids.CREATOR_ALL_ACL, CreateMode.PERSISTENT);
             zoo.create("/backup", "znode".getBytes(), ZooDefs.Ids.CREATOR_ALL_ACL, CreateMode.PERSISTENT);
         } catch (KeeperException.NodeExistsException ignored) { }
-
-        // add all necessary watchers
-        setWatchers();
 
     }
 
@@ -98,16 +94,16 @@ class Master{
      * @throws KeeperException -
      * @throws InterruptedException -
      */
-    static void deleteSubtree(String rootPath)  throws KeeperException, InterruptedException {
+    static void deleteSubtree(String rootPath) throws KeeperException, InterruptedException {
 
         // If non-existing node => warns and exits
-        if(zoo.exists(rootPath, false) == null) {
+        if(!ZooHelper.exists(rootPath, zoo)) {
             ZooHelper.print("<WARNING> trying to delete a NON-existing node in path " + rootPath + ".");
             return;
         }
 
         // Look for children of the given path, null if there is no child
-        List<String> children = zoo.getChildren(rootPath, false); // False => synchronous process, no watcher to manage sync
+        List<String> children = zoo.getChildren(rootPath, false);
 
         // Node is a leaf iff children == null
         for (String aChildren : children) deleteSubtree(rootPath + '/' + aChildren);
@@ -115,21 +111,6 @@ class Master{
         // when all the children and their subtrees are deleted, delete also the root
         zoo.delete(rootPath, -1);
 
-    }
-
-    static void getOnlineUsers() throws KeeperException, InterruptedException, IOException {
-        List<String> onlineIds = ZooHelper.getConnection().getChildren("/online", false);
-
-        if (onlineIds.size() == 0){
-            System.out.println("[ No Online Users ]");
-            return;
-        }
-
-        System.out.print("[ Online Users: ");
-        for (String id : onlineIds) {
-            System.out.print(id + " ");
-        }
-        System.out.println("]");
     }
 
     /**
@@ -147,6 +128,18 @@ class Master{
             zoo.getChildren("/online", watcher);
 
         } catch (Exception e) { e.printStackTrace(); }
+
+    }
+
+    private static void manageUnprocessedRequests() throws KeeperException, InterruptedException {
+
+        // handle quit requests first
+        List<String> unprocessedQuittingIds = zoo.getChildren("/request/quit", false,null);
+        for (String id : unprocessedQuittingIds) watcher.handleQuitRequest(id);
+
+        // handle enrollment requests
+        List<String> unprocessedEnrollingIds = zoo.getChildren("/request/enroll", false,null);
+        for (String id : unprocessedEnrollingIds) watcher.handleEnrollRequest(id);
 
     }
 
